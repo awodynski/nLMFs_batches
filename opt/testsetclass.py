4f0da5a3f94d05cf0a57c5e99fbe0eccd311f3d2
awodynski <artur.wodynski@tu-berlin.de>
2025-08-28 20:12:22 +0200

    Update testsetclass.py

--- opt/testsetclass.py
+++ opt/testsetclass.py
@@ -3,12 +3,9 @@ import subprocess
 
 class TestsetClass:
     def run_dftd4(self,  a1, a2, coord_path, molecule_charge):
-        #print(molecule_charge)
-        #print(coord_path)
         command_s6 = f"/homes2/chemie/wodynski/bin/panda6/bin/dftd4 --charge {molecule_charge} --input coord --param 1.0 0.0 {a1} {a2} --mbdscale 0.0 --silent {coord_path} | grep 'Dispersion energy:' | awk '{{print $3}}'"
         command_s8 = f"/homes2/chemie/wodynski/bin/panda6/bin/dftd4 --charge {molecule_charge} --input coord --param 0.0 1.0 {a1} {a2} --mbdscale 0.0 --silent {coord_path} | grep 'Dispersion energy:' | awk '{{print $3}}'"
         command_s9 = f"/homes2/chemie/wodynski/bin/panda6/bin/dftd4 --charge {molecule_charge} --input coord --param 0.0 0.0 {a1} {a2} --mbdscale 1.0 --silent {coord_path} | grep 'Dispersion energy:' | awk '{{print $3}}'"
-        # Uruchomienie polecenia w powłoce i przechwycenie wyjścia
         result_s6 = subprocess.run(command_s6, shell=True, text=True, capture_output=True)
         result_s8 = subprocess.run(command_s8, shell=True, text=True, capture_output=True)
         result_s9 = subprocess.run(command_s9, shell=True, text=True, capture_output=True)
